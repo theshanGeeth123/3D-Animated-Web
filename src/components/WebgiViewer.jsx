be172f5d1e8aa809acd1d9c5b36ae2e78ee5f4f8
Theshan <theshangeethanjana@gmail.com>
2026-01-11 15:56:04 +0530

    Animation 1 properly working

--- src/components/WebgiViewer.jsx
+++ src/components/WebgiViewer.jsx
@@ -11,52 +11,60 @@ import {
     GammaCorrectionPlugin,
     addBasePlugins,
     mobileAndTabletCheck,
-    CanvasSnipperPlugin 
+    CanvasSnipperPlugin
 } from "webgi";
 
 import gsap from "gsap"
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { scrollAnimation } from '../lib/scroll-animation.js'
+
+gsap.registerPlugin(ScrollTrigger);
 
 function WebgiViewer() {
 
     const convasRef = useRef(null);
 
+    const memorizeScrollAnimation = useCallback(
+        (position,target,onUpdate) => {
+
+            if(position && target && onUpdate){
+                scrollAnimation(position,target,onUpdate);
+            }
+
+        },[]
+    )
+
     const setupViewer = useCallback(async () => {
 
-        // Initialize the viewer
         const viewer = new ViewerApp({
             canvas: convasRef.current,
         })
 
-        // Add some plugins
         const manager = await viewer.addPlugin(AssetManagerPlugin)
 
+        const camera = viewer.scene.activeCamera;
+
+        const position = camera.position;
+
+        const target = camera.target;
 
         // Add plugins individually.
-        // await viewer.addPlugin(GBufferPlugin)
-        // await viewer.addPlugin(new ProgressivePlugin(32))
-        // await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
-        // await viewer.addPlugin(GammaCorrectionPlugin)
-        // await viewer.addPlugin(SSRPlugin)
-        // await viewer.addPlugin(SSAOPlugin)
-        // await viewer.addPlugin(DiamondPlugin)
-        // await viewer.addPlugin(FrameFadePlugin)
-        // await viewer.addPlugin(GLTFAnimationPlugin)
-        // await viewer.addPlugin(GroundPlugin)
-        // await viewer.addPlugin(BloomPlugin)
-        // await viewer.addPlugin(TemporalAAPlugin)
-        // await viewer.addPlugin(AnisotropyPlugin)
+        await viewer.addPlugin(GBufferPlugin)
+        await viewer.addPlugin(new ProgressivePlugin(32))
+        await viewer.addPlugin(new TonemapPlugin(true))
+        await viewer.addPlugin(GammaCorrectionPlugin)
+        await viewer.addPlugin(SSRPlugin)
+        await viewer.addPlugin(SSAOPlugin)
+        await viewer.addPlugin(BloomPlugin)
 
-        // or use this to add all main ones at once.
-        await addBasePlugins(viewer)
 
-        // Add more plugins not available in base, like CanvasSnipperPlugin which has helpers to download an image of the canvas.
-        await viewer.addPlugin(CanvasSnipperPlugin)
 
         // This must be called once after all plugins are added.
-        viewer.renderer.refreshPipeline()
+        viewer.renderer.refreshPipeline();
 
-        await manager.addFromPath("scene-black.glb")
+        await manager.addFromPath("scene-black.glb");
+
+        viewer.getPlugin(TonemapPlugin).config.clipBackground = true
 
         // Load an environment map if not set in the glb file
         // await viewer.scene.setEnvironment(
@@ -65,13 +73,36 @@ function WebgiViewer() {
         //     )
         // );
 
+        viewer.scene.activeCamera.setCameraOptions({ controlsEnabled: false });
 
+        window.scrollTo(0, 0);
+
+        let needsUpdate = true;
+
+        const onUpdate = () => {
+
+            needsUpdate = true;
+            viewer.setDirty();
+            
+        }
+
+        viewer.addEventListener("preFrame", () => {
+
+            if (needsUpdate) {
+                camera.positionTargetUpdated(true);
+                needsUpdate = false;
+            }
+
+
+        })
+
+        memorizeScrollAnimation(position,target,onUpdate);
 
     }, [])
 
     useEffect(() => {
         setupViewer();
-    },[])
+    }, [])
 
     return (
         <div id='webgi-canvas-container'>
